684cf02d72d73b2fcdf5d18023e550c7c29e917a
andreu.boix.drago <andreu.boix.drago@auleseebe.upc.edu>
2024-09-26 11:11:00 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,41 @@
 #Codigo para problema 2
+iris
+mis_dadas <- iris
+
+y <- mis_dadas$Sepal.Length
+y
+
+x <- mis_dadas$Petal.Length
+x
+
+plot(x,y)
+
+xbar = mean(x)
+xbar
+
+ybar= mean(y)
+ybar
+
+m=sum((x-xbar)*(y-ybar))/sum((x-xbar)^2)
+m
+b= ybar-m*xbar
+b
+pred= b+m*1.5
+pred
+
+mod=lm(y~x)
+
+ypredicted=predict(mod,data.frame(x=x))
+ypredicted
+
+plot(x,ypredicted, type="l")
+points(x,y)
+plot(x,y, pch=16, col="red")
+lines(x, ypredicted)
+
+Rsq = sum((ypredicted-ybar)^2)/sum((y-ybar)^2)
+Rsq
+
+summary(mod)
+sqrt(Rsq)
+cor.test(x,y)
